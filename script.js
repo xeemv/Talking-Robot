2bae2c7147ccb16fb62352c2a2dd41c90a5ca93f
xeemv <xeeyang08@gmail.com>
2021-12-22 16:07:47 -0600

    working on robot js

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+
+const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition; // to recognize speech
+
+const recognition = new speech
+
+
+
+
+button.addEventListener("click", ()=> { // this for when someone selects on the button
+
+})
