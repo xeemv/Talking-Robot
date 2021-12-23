a9265aa8717aa75f82c51b76c19a139982753b73
xeemv <xeeyang08@gmail.com>
2021-12-22 22:34:09 -0600

    updated js, speech rec works

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const button = document.querySelector("button");
 
 const SpeechRecognition = 
-    window.SpeechRecognition || window.webkitSpeechRecognition; // to recognize speech
+    window.SpeechRecognition || window.webkitSpeechRecognition; 
+    /* 
+    - To get the speech recognition of the browser
+    - Do take into account that FireFox does not support text-to-speech/webspeech api
+    - cited by: https://wiki.mozilla.org/Web_Speech_API_-_Speech_Recognition
+        - "Chrome, Edge, Safari and Opera support a form of this API currently for Speech-to-text, which means sites that rely on it work in those browsers, but not in Firefox."
+    */
 
 const recognition = new SpeechRecognition();
 
-recognition.onStart = function() {
+recognition.onstart = function () {
     console.log("Speech Recognition started!");
 };
 
-recognition.onResult = function (event) {
+recognition.onresult = function (event) {
     console.log(event);
 };
 
-button.addEventListener("click", () => { // this for when someone selects on the button
+button.addEventListener("click", () => { 
     recognition.start();
 });
+// this is a function for when someone selects on the button
